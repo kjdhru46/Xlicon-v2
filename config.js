391436753d9256667a22eb3f81b99e2425a70a49
kjdhru46 <144645709+kjdhru46@users.noreply.github.com>
2023-09-19 11:20:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,11 +17,11 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["8801853262586","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["8801853262586","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.ownertag = ["8801928502256"];
-global.OwnerName = "Slasher";
-global.BotName = "XLICON-MD";
+global.Owner = ["15194881878"];         //If you want singal number so global.Owner = ['15194881878'] Change into your number.
+global.OwnerNumber = ["15194881878"];   //If you want singal number so global.Owner = ['15194881878'] Change into your number.
+global.ownertag = ["15194881878"];
+global.OwnerName = "Yohan";
+global.BotName = "J.A.V.I.S 🤖";
 global.packname = "X-2.0 Bot";                             //Do not change.
 global.author = "By: Slasher";                               //Do not change.
 global.BotSourceCode = "https://github.com/ahil15/Xlicon-v2"; //Do not change.
